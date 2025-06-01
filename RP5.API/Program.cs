8beda8ab43d1a34c7147ae62d2a1d27f6b843f3c
cust0me <cust0me@custome.pw>
2025-06-01 20:54:40 +0200

    Refactor Docker Compose configuration to improve service health checks and streamline InfluxDB setup

--- RP5.API/Program.cs
+++ RP5.API/Program.cs
@@ -19,11 +19,8 @@ builder.Services.AddHostedService<MqttBackgroundService>();
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
